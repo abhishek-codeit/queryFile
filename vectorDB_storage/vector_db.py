39ba0e2db81b8459a95ce45125ebb7762d00c685
Abhishek S <76235557+abhishek-codeit@users.noreply.github.com>
2026-04-26 13:46:47 +0530

    Added Qdrant vectorDB storage upsert and search function

--- vectorDB_storage/vector_db.py
+++ vectorDB_storage/vector_db.py
@@ -2,7 +2,7 @@ from qdrant_client import QdrantClient
 from qdrant_client.models import VectorParams, Distance, PointStruct
 
 class QdrantStorage:
-    def __init__(self,url="http://localhost:6333",collection="docs",dim=3072)
+    def __init__(self,url="http://localhost:6333",collection="docs",dim=3072):
         self.client = QdrantClient(url=url, timeout=30)
         self.collection = collection
         if not self.client.collection_exists(self.collection):
@@ -39,5 +39,4 @@ class QdrantStorage:
                 sources.add(source)
 
             return {"contexts": contexts, "sources": sources}
-        
         
